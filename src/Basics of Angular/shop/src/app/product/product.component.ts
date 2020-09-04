0e604cdf60e5b4401b6eb1293bf4383898a5542f
cihat <cihat02020202@gmail.com>
2020-09-04 16:11:36 +0300

    products component screen printing

--- src/Basics of Angular/shop/src/app/product/product.component.ts
+++ src/Basics of Angular/shop/src/app/product/product.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  constructor() {}
+  title = 'Ürün Listesi';
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 2500,
+      categoryId: 1,
+      description: 'Asus Zenbook',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+    },
+    {
+      id: 2,
+      name: 'Mouse',
+      price: 25,
+      categoryId: 2,
+      description: 'A4 Tech',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496878632226-93afc36151ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 2500,
+      categoryId: 1,
+      description: 'Asus Zenbook',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+    },
+    {
+      id: 2,
+      name: 'Mouse',
+      price: 25,
+      categoryId: 2,
+      description: 'A4 Tech',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496878632226-93afc36151ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 2500,
+      categoryId: 1,
+      description: 'Asus Zenbook',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+    },
+    {
+      id: 2,
+      name: 'Mouse',
+      price: 25,
+      categoryId: 2,
+      description: 'A4 Tech',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496878632226-93afc36151ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 2500,
+      categoryId: 1,
+      description: 'Asus Zenbook',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+    },
+    {
+      id: 2,
+      name: 'Mouse',
+      price: 25,
+      categoryId: 2,
+      description: 'A4 Tech',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496878632226-93afc36151ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
