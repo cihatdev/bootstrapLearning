2c090a8eb27aa039ef147908915fdcddfa0b5e08
cihatdev <cihatsalik1@gmail.com>
2020-09-18 13:32:36 +0300

    Updates

---
+++ precache-manifest.9d84dc2d10490a50aace98d030281b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783e03c3f8eb4e296d307ae4a9e593cf",
+    "url": "/fronEnd-Dev/index.html"
+  },
+  {
+    "revision": "6e16ca59fa65cf226540",
+    "url": "/fronEnd-Dev/static/css/2.8fe03e69.chunk.css"
+  },
+  {
+    "revision": "6e16ca59fa65cf226540",
+    "url": "/fronEnd-Dev/static/js/2.591cc438.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/fronEnd-Dev/static/js/2.591cc438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a130666f0b6da2ad0b80",
+    "url": "/fronEnd-Dev/static/js/main.3904628c.chunk.js"
+  },
+  {
+    "revision": "60c84372ac33a32de959",
+    "url": "/fronEnd-Dev/static/js/runtime-main.38fcaaf0.js"
+  }
+]);
